f4b9b3fe0b885a0cb3e3a1dfdf3e71e553f49c82
KentFujii <kent.where.the.light.is@gmail.com>
2019-06-23 00:31:57 +0900

    wip: internationalized

---
+++ app/controllers/InternationalizedAppController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n._
+
+@Singleton
+class InternationalizedAppController @Inject()(cc: ControllerComponents, messagesApi: MessagesApi) extends AbstractController(cc) with I18nSupport {
+  val enquiryForm = Form(
+    tuple(
+      "emailId" -> email,
+      "userName" -> optional(text),
+      "question" -> nonEmptyText
+    )
+  )
+
+  def index = Action { implicit request =>
+    Redirect(routes.InternationalizedAppController.askUs)
+  }
+
+  def askUs = Action { implicit request =>
+    Ok(views.html.internationalized_app.index(enquiryForm))
+  }
+
+  def enquire = Action { implicit request =>
+    enquiryForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.internationalized_app.index(errors)),
+      query => {
+        println(query.toString)
+        Redirect(routes.InternationalizedAppController.askUs)
+      }
+    )
+  }
+}
